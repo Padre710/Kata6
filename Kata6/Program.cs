7c204c41ded1328d156dabe62a96532d7a13a5ff
23490 <23490@TREASURE>
2022-09-03 09:29:43 +0100

    Add project files.

---
+++ Kata6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//You are given an array (which will have a length of at least 3
+//, but could be very large) containing integers.
+//The array is either entirely comprised of odd integers or entirely comprised of 
+//even integers except for a single integer N. Write a method that takes the array 
+//as an argument and returns this "outlier" N.
+
+//Examples
+//[2, 4, 0, 100, 4, 11, 2602, 36]
+//Should return: 11 (the only odd number)
+
+//[160, 3, 1719, 19, 11, 13, -21]
+//Should return: 160(the only even number)
+
+namespace Kata6
+
+{
+
+    class Program
+    {
+        public  class Kata
+        {
+            public static int Find(int[] integers)
+            {
+                
+
+                List<int> even = new List<int>();
+                List<int> odd = new List<int>();
+                foreach (var num in integers)
+                { 
+                    if (num % 2 == 0)
+                    {
+                        even.Add(num);
+                    }
+                    else
+                    {
+                        odd.Add(num);
+                    }
+                }
+                    int[] even_no = even.ToArray();
+                    int[] odd_no = odd.ToArray();
+                if (even_no.Length == 1)
+                {
+                    return even_no[0];
+                }
+                else
+                    return odd_no[0];
+
+
+               
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] numbers =  { 160, 3, 1719, 19, 11, 13, -21 };
+            Console.WriteLine(Kata.Find(numbers));
+        }
+    }
+}
